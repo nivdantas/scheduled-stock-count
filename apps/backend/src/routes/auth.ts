1205d05c776417cd09f4f36854d1c61164828e21
NivDantas <dantasniv@gmail.com>
2026-02-12 06:51:19 -0300

    Fix cookies strict permission

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -42,7 +42,7 @@ router.post("/login", async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       path: "/",
     });
 
@@ -56,7 +56,7 @@ router.post("/logout", (req, res) => {
   res.clearCookie("stock_token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     path: "/",
   });
   return res.json({ message: "Deslogado com sucesso" });
